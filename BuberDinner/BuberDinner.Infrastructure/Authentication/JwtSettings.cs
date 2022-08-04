4386728b7e298ffabaac51cfa669ef1832dee8fb
Otávio Anselmo <otavio.aanselmo@gmail.com>
2022-08-04 15:03:38 -0300

    object mapping with mapster and remove null from string entities

--- BuberDinner/BuberDinner.Infrastructure/Authentication/JwtSettings.cs
+++ BuberDinner/BuberDinner.Infrastructure/Authentication/JwtSettings.cs
@@ -3,9 +3,9 @@ namespace BuberDinner.Infrastructure.Authentication
     public class JwtSettings
     {
         public const string SectionName = "JwtSettings";
-        public string Secret { get; init; } = null;
+        public string Secret { get; init; } = string.Empty;
         public int ExpiryMinutes { get; init; }
-        public string Issuer { get; init; } = null;
-        public string Audience { get; init; } = null;
+        public string Issuer { get; init; } = string.Empty;
+        public string Audience { get; init; } = string.Empty;
     }
 }
