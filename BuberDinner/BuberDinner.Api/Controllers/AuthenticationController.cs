6ec1c3ce9611c092ba5d0a6cbd93dcc35f87ab7d
Otávio Anselmo <otavio.aanselmo@gmail.com>
2022-09-07 16:43:19 -0300

    dinnerscontroller and adding jwt bearer authentication and authorization in all controllers

--- BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
+++ BuberDinner/BuberDinner.Api/Controllers/AuthenticationController.cs
@@ -6,11 +6,13 @@ using BuberDinner.Domain.Common.Errors;
 using ErrorOr;
 using MapsterMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers;
 
 [Route("auth")]
+[AllowAnonymous]
 
 public class AuthenticationController : ApiController
 {
